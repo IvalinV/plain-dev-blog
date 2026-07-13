fcee1efeac0ae63adb92ebac1956bf7eb70aaf5d
Ivalin Venkov <ivalinvenkov@gmail.com>
2026-07-13 14:42:16 +0300

    Implemented favicon

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -16,6 +16,13 @@
     <meta name="description" content="{{ $metaDescription }}">
     <link rel="canonical" href="{{ url()->current() }}">
 
+    {{-- Favicons --}}
+    <link rel="icon" href="{{ asset('favicon/favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+
     {{-- Open Graph --}}
     <meta property="og:site_name" content="{{ $siteName }}">
     <meta property="og:type" content="@yield('og_type', 'website')">
