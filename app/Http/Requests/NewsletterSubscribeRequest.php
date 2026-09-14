16d28f1f3b47ef359d0c5d8fd41feb4a4c339273
Ivalin Venkov <ivalinvenkov@gmail.con>
2026-09-14 18:13:42 +0300

    Feat: Subscribe to newsletter - new form for subscribing to newsletter using spatie/laravel-newsletter package and MailerLite API

---
+++ app/Http/Requests/NewsletterSubscribeRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Override;
+
+class NewsletterSubscribeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email:rfc,dns'],
+        ];
+    }
+
+    #[Override]
+    public function messages(): array
+    {
+        return ['email.required' => 'Email should be valid email address'];
+    }
+}
